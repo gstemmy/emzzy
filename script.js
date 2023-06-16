293c6c04256a46ad5729cd4fc3fbd0e0570899e5
gstemmy <gbolahansaheedt@gmail.com>
2023-06-16 22:38:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let formbtn = document.querySelector("#transfer");
+let backbtn = document.querySelector("#back");
+let middlecontainer = document.querySelector(".ticket");
+let middlecontainer2 = document.querySelector(".ticket01");
+let middlecontainer1 = document.querySelector(".ticket02");
+
+
+window.addEventListener("load", function (){
+    formbtn.addEventListener("click", function() {
+        middlecontainer.style.display = "block";
+        setTimeout(function() {
+            middlecontainer.style.display = "none";
+            middlecontainer1.style.display = "none";
+            middlecontainer2.style.display = "block";
+        }, 2000);
+    });
+});
+
+
+
+
+
+backbtn.onclick = () =>{
+   middlecontainer2.style.display = "none";
+   middlecontainer.style.display = "none";
+   middlecontainer1.style.display = "block";
+
+}
